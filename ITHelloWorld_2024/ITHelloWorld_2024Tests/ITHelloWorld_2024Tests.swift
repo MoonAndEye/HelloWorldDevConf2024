615435a4570b7ddf9685521c135cc0a08a71c558
Marvin <atimis19@gmail.com>
2024-08-27 23:09:04 +0800

    project init

---
+++ ITHelloWorld_2024/ITHelloWorld_2024Tests/ITHelloWorld_2024Tests.swift
@@ -0,0 +1,16 @@
+//
+//  ITHelloWorld_2024Tests.swift
+//  ITHelloWorld_2024Tests
+//
+//  Created by Marvin on 2024/9/1.
+//
+
+import Testing
+
+struct ITHelloWorld_2024Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
