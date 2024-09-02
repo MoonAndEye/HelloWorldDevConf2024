2efbd3ab11bb2f3374694cbf257baf748efefaf2
Marvin <atimis19@gmail.com>
2024-09-02 23:19:33 +0800

    project init

--- ITHelloWorld_2024/ITHelloWorld_2024/ITHelloWorld_2024App.swift
+++ ITHelloWorld_2024/ITHelloWorld_2024/ITHelloWorld_2024App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ITHelloWorld_2024App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FeatureListView()
         }
     }
 }
---
+++ ITHelloWorld_2024/ITHelloWorld_2024/Views/FeatureListView.swift
@@ -0,0 +1,46 @@
+//
+//  FeatureListView.swift
+//  ITHelloWorld_2024
+//
+//  Created by Marvin on 2024/9/1.
+//
+
+import SwiftUI
+
+struct FeatureListView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+#if canImport(UIKit)
+                NavigationLink() {
+                    WritingToolDemo()
+                } label: {
+                    Text("Writing Tool Demo")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+#endif
+                NavigationLink {
+                    WritingToolDemoSwiftUI()
+                } label: {
+                    Text("Wrtiting Tool Demo SwiftUI")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("功能列表").font(.headline).frame(maxWidth: .infinity)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    FeatureListView()
+}
